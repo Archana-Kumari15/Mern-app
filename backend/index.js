8a694205394d07d28a1b1cf70770002148224359
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-06-23 14:21:06 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Router/AuthRouter');
+const productRouter = require('./Router/productRouter');
+
+require('dotenv').config();
+require('./models/db')
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping',(req,res) => {
+    res.send('PONG');
+})
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', productRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
